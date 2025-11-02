e2cf112cbf7f4b9fe493b1a3d97a48be8c1ba1e8
Lee <ldolse@yahoo.com>
2025-11-02 12:22:04 +0800

    Update md5_handler.py

--- rom_management/handlers/md5_handler.py
+++ rom_management/handlers/md5_handler.py
@@ -66,3 +66,8 @@ class MD5ScanHandler(SpecialHandler):
     def _requires_user_intervention(self, error: Exception) -> bool:
         """MD5 scan always requires user intervention"""
         return isinstance(error, MD5ScanRequiredException)
+
+    def validate_preconditions(self, media: CDMedia, file_data = None) -> bool:
+        """Check if this handler should be applied"""
+        dat = media.dat_game_entry.dat
+        return any(hasattr(rom, 'md5') and not hasattr(rom, 'crc') for rom in dat.roms)
